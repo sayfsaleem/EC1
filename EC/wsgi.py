64fd9a5bcbc8138fa5b1dbf08c7471b80c238ca7
sayfsaleem <114619134+sayfsaleem@users.noreply.github.com>
2024-03-29 14:47:06 +0500

    Update wsgi.py

--- EC/wsgi.py
+++ EC/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EC.settings')
 
 # Get the Django WSGI application
-django_app = get_wsgi_application()
+django_application = get_wsgi_application()
 
 # Wrap the Django application with WhiteNoise
